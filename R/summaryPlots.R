371099d2ddb179303ac819d57b97c3d37913f9c0
Parker Knight <pknight24@protonmail.com>
2020-03-08 15:29:08 -0400

    Fix percentC plot

--- R/summaryPlots.R
+++ R/summaryPlots.R
@@ -13,21 +13,27 @@ percent_C <- function(orderObject){
     c.yellow <- sapply(yellow.sites, function(i){
         sum(dat[, i] == -4) / nrow(dat)
     })
-    plot(x = red.sites - ncol(dat)/2, y = c.red, col="red", pch=19)
+    plot(x = red.sites - ncol(dat)/2, y = c.red,
+         col="red", pch=19, ylim=c(0,1), xlab = "Site Number", ylab="%C")
+    lines(x = red.sites - ncol(dat)/2, y = c.red, col="red")
     points(x = yellow.sites, y = c.yellow, col="yellow", pch=19)
+    lines(x = yellow.sites, y = c.yellow, col="yellow")
+    n.sites <- length(union(red.sites, yellow.sites))
+    labs <- union(red.sites, yellow.sites)[seq(1, n.sites, by=n.sites/12)]
+    #axis(1, at=labs)
 }
 
 # the proportion of a given color at each read, histogram of proportions
 # color defaults to yellow
 proportion_color <- function(orderObject, color = "YELLOW", plotHistogram=FALSE){
   color.indicator <- ifelse(color=="YELLOW", -1, 1)
-  proportion <- apply(orderObject$toClust, 1, function(x){
+  Proportion <- apply(orderObject$toClust, 1, function(x){
     sum(x == color.indicator * 3 | x == color.indicator * 4) / (length(x) / 2)
   })
-  if (plotHistogram) hist(proportion,
+  if (plotHistogram) hist(Proportion,
                           main=paste("Proportion of", ifelse(color.indicator==-1, "Yellow", "Red"), "per Read"),
                           xlim=c(0,1))
 
-  return(proportion)
+  return(Proportion)
 }
 
