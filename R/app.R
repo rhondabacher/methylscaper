1f255660117333c6e82c289006723d5c4161c0ab
Rhonda Bacher <rhondabacher@gmail.com>
2021-04-02 23:36:40 -0400

    update namespace

--- R/app.R
+++ R/app.R
@@ -6,6 +6,7 @@
 #' @return This starts up the shiny app interface for methylscaper.
 #' @import shiny
 #' @import seriation
+#' @importFrom seqinr read.fasta
 #' @importFrom shinyjs toggleElement showElement
 #' @importFrom shinyFiles shinyDirChoose shinyDirButton getVolumes
 #' @importFrom grDevices dev.off pdf png
