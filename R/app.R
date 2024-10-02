8124591c89d80036960fc74d012be0dee3b3c2f7
bahetyshyam <bahetyshyam@gmail.com>
2024-10-01 22:58:09 -0400

    Updated app.R file to support production deployment

--- R/app.R
+++ R/app.R
@@ -16,8 +16,20 @@
 #' @examples
 #'
 #' # methylscaper()
-methylscaper <- function() {
-    options(shiny.maxRequestSize = 10000 * 1024^5)
+methylscaper <- function(port = 19903, is_local_development = TRUE) {
+    # default port is set to 19903
+    # if is_local_development is set to false, then a browser window will
+    # not open up. We should use FALSE in production server
+    options_args <- list(
+        shiny.maxRequestSize = 10000 * 1024^5,
+        shiny.launch.browser = is_local_development,
+        shiny.port = port,
+        test.mode = getOption("shiny.testmode", FALSE)
+    )
+    if (is_local_development == FALSE) {
+        options_args$shiny.host <- "0.0.0.0"
+    }
+    do.call(options, options_args)
 
     shinyApp(ui = ui, server = server)
 }
